9a51b362a388950094ad537ccbc1a6591ae78755
Mayank1343 <mayanksharma1343@gmail.com>
2025-12-20 14:45:33 +0530

    final upload for backend deployment

--- blood-donation-api/src/main/java/config/WebConfig.java
+++ blood-donation-api/src/main/java/config/WebConfig.java
@@ -14,9 +14,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://superlative-marshmallow-9c48ab.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
+
             }
         };
     }
